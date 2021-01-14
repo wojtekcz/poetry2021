148ed3147c035e652d15c6f901bf7f546e353fb5
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-14 10:17:55 +0100

    Install stemmer in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,11 @@ COPY environment.yml .
 RUN conda env update --name base --file environment.yml 
 RUN pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
+## setup stemmer
+WORKDIR /usr/local
+RUN wget -nv --no-clobber https://github.com/wojtekcz/poetry2021/releases/download/v0.1/stemmer-2.0.3.tgz \
+    && tar xzf stemmer-2.0.3.tgz && rm stemmer-2.0.3.tgz 
+
 WORKDIR /workspace
 EXPOSE 8888
 CMD [ "jupyter", "lab", "--allow-root", "--ip=0.0.0.0", "--no-browser" ]
--- preprocessing/stemmer.py
+++ preprocessing/stemmer.py
@@ -9,5 +9,5 @@ class Stemmer:
     def stem_file(fn_corpus_caps: Path, fn_corpus_syl: Path, s_opts=7683):
         platform_suffixes = {'Linux': 'linux', 'Darwin': 'macos'}
         platform_suffix = platform_suffixes[platform.system()]
-        stemmer_bin = f'LD_PRELOAD="" bin/stemmer.{platform_suffix}'
-        os.system(f'{stemmer_bin} -s {s_opts} -d bin/stemmer2.dic -i {fn_corpus_caps} -o {fn_corpus_syl}')
+        stemmer_bin = f'LD_PRELOAD="" /usr/local/bin/stemmer.{platform_suffix}'
+        os.system(f'{stemmer_bin} -s {s_opts} -d /usr/local/bin/stemmer2.dic -i {fn_corpus_caps} -o {fn_corpus_syl}')
