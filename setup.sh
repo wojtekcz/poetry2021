63ada0ec5dd211e905ee84b808407bebd851175c
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-15 09:47:25 +0100

    Setting up project for colab

--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
-# !bash <(curl -s https://raw.githubusercontent.com/wojtekcz/language2motion/master/notebooks/Colab/swift_colab_ssh_server_bekaes.sh)
-
-
-## get dataset
+# docker setup
+## download dataset
 dataset_path=/workspace/data/pan_tadeusz
 mkdir -p $dataset_path
 wget --no-clobber -P $dataset_path https://github.com/wojtekcz/poetry2021/releases/download/v0.1/pan_tadeusz.txt
 
+# colab setup
+
+## ssh tunnel
+# upload private_key.pem and authorized_keys to /content
+# !SSH_HOST=<user>@<host> <(curl -s https://raw.githubusercontent.com/wojtekcz/poetry2021/master/colab_ssh/swift_colab_ssh_server_bekaes.sh)
+
 ## setup stemmer
-# wget --no-clobber https://github.com/wojtekcz/poetry2021/releases/download/v0.1/stemmer-2.0.3.tgz
-# tar xzf stemmer-2.0.3.tgz
-# rm stemmer-2.0.3.tgz
+cd /usr/local
+wget --no-clobber -q https://github.com/wojtekcz/poetry2021/releases/download/v0.1/stemmer-2.0.3.tgz
+tar xzf stemmer-2.0.3.tgz
+rm stemmer-2.0.3.tgz
+
+## project sources setup
+cd /content
+git clone https://github.com/wojtekcz/poetry2021.git
+
+## download dataset
+dataset_path=/content/poetry2021/data/pan_tadeusz
+mkdir -p $dataset_path
+wget --no-clobber -P $dataset_path https://github.com/wojtekcz/poetry2021/releases/download/v0.1/pan_tadeusz.txt
