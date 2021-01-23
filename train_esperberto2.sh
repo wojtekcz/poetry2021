24c392ef11a079d49dc5a9dc871d7aa23df21f25
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-23 07:17:54 +0100

    Setting up esperberto training with run_language_modeling.py

---
+++ train_esperberto2.sh
@@ -0,0 +1,14 @@
+python3 run_language_modeling.py \
+    --output_dir ./models/EsperBERTo-small-v1 \
+    --model_type roberta \
+    --mlm \
+    --config_name ./models/EsperBERTo-small \
+    --tokenizer_name ./models/EsperBERTo-small \
+    --do_train \
+    --do_eval \
+    --learning_rate 1e-4 \
+    --num_train_epochs 5 \
+    --save_total_limit 2 \
+    --save_steps 2000 \
+    --per_gpu_train_batch_size 16 \
+    --seed 42
---
+++ train_esperberto_tokenizer.py
@@ -0,0 +1,26 @@
+#! pip install tokenizers
+
+from pathlib import Path
+from tokenizers import ByteLevelBPETokenizer
+
+data_path = Path('data/esperberto')
+dataset_path = data_path / 'dataset'
+tokenizer_path = data_path / 'tokenizer2'
+
+paths = [str(dataset_path / 'oscar.eo.1000.txt')]
+
+# Initialize a tokenizer
+tokenizer = ByteLevelBPETokenizer()
+
+# Customize training
+tokenizer.train(files=paths, vocab_size=10_000, min_frequency=2, special_tokens=[
+    "<s>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
+
+# Save files to disk
+tokenizer_path.mkdir(parents=True, exist_ok=True)
+tokenizer.save_model(str(tokenizer_path))
