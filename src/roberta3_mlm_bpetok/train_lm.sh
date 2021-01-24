ca4f2472e43e8dce4d8eff3c40bf9c6a5e0e8956
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2021-01-24 22:32:46 +0000

    run_3

--- src/roberta3_mlm_bpetok/train_lm.sh
+++ src/roberta3_mlm_bpetok/train_lm.sh
@@ -1,5 +1,5 @@
 home_dir=/root/poetry2021
-run=2
+run=3
 PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
     --output_dir ${home_dir}/runs/pan_tadeusz2/run_${run} \
     --logging_dir ${home_dir}/runs/pan_tadeusz2/run_${run}_logs \
@@ -8,9 +8,10 @@ PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
     --config_name ${home_dir}/data/pan_tadeusz2/model_config \
     --tokenizer_name ${home_dir}/data/pan_tadeusz2/tokenizer \
     --max_seq_length 128 \
+    --line_by_line \
     --train_file ${home_dir}/data/pan_tadeusz2/dataset/pan_tadeusz.x100.txt \
     --do_train \
-    --learning_rate 1e-4 \
+    --learning_rate 1e-3 \
     --per_device_train_batch_size 1000 \
     --num_train_epochs 650 \
     --seed 42 \
@@ -25,5 +26,4 @@ PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
     --per_device_eval_batch_size 800
 
     # --disable_tqdm False
-    # --line_by_line \
     # --per_device_train_batch_size 2000 \
