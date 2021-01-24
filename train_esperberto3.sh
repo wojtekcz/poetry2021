0bd2ae5969e2543d659d8f9eb683b78ac8a4522f
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2021-01-24 14:23:51 +0000

    Setting up run_1

--- train_esperberto3.sh
+++ train_esperberto3.sh
@@ -1,5 +1,7 @@
 python3 run_mlm.py \
     --output_dir ./runs/esperberto3/run_1 \
+    --logging_dir ./runs/esperberto3/run_1_logs \
+    --overwrite_output_dir \
     --model_type roberta \
     --config_name ./data/esperberto3/model_config \
     --tokenizer_name ./data/esperberto3/tokenizer \
@@ -7,12 +9,13 @@ python3 run_mlm.py \
     --max_seq_length 128 \
     --train_file ./data/esperberto3/dataset/oscar.eo.1000x10.txt \
     --do_train \
+    --fp16 \
+    --learning_rate 1e-3 \
+    --per_device_train_batch_size 1300 \
+    --num_train_epochs 40 \
+    --seed 42 \
+    --logging_steps 10 \
     --disable_tqdm False
 
-    # --fp16 \
-    # --learning_rate 1e-4 \
-    # --num_train_epochs 5 \
     # --save_total_limit 2 \
     # --save_steps 2000 \
-    # --per_device_train_batch_size 16 \
-    # --seed 42
