f9aee13812567242a352661c39c7ae51dcdece23
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-24 14:53:03 +0100

    Setting up esperberto training with run_mlm.py, cont.d.

--- run_mlm.py
+++ run_mlm.py
@@ -265,7 +265,9 @@ def main():
         "use_auth_token": True if model_args.use_auth_token else None,
     }
     if model_args.tokenizer_name:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
+        from transformers import RobertaConfig
+        tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, config=RobertaConfig(), **tokenizer_kwargs)
+        # tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
     elif model_args.model_name_or_path:
         tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
     else:
