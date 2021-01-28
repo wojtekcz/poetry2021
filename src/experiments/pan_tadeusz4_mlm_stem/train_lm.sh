82e78437d77097ffbf5f13ffc8ed835ef98fb877
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-28 19:14:10 +0000

    Setting up training

---
+++ src/experiments/pan_tadeusz4_mlm_stem/train_lm.sh
@@ -0,0 +1,30 @@
+home_dir=/root/poetry2021
+# home_dir=/workspace/poetrśy2021.gt
+run=1
+PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
+    --output_dir ${home_dir}/runs/ch/run_${run} \
+    --logging_dir ${home_dir}/runs/pan_tadeusz4/run_${run}_logs \
+    --overwrite_output_dir \
+    --model_type roberta \
+    --config_name ${home_dir}/data/pan_tadeusz4/model_config \
+    --tokenizer_name ${home_dir}/data/pan_tadeusz4/tokenizer \
+    --max_seq_length 128 \
+    --line_by_line \
+    --train_file ${home_dir}/data/pan_tadeusz4/dataset/pan_tadeusz.syl1.x100.txt \
+    --do_train \
+    --fp16 \
+    --learning_rate 1e-3 \
+    --per_device_train_batch_size 1000 \
+    --num_train_epochs 650 \
+    --seed 42 \
+    --save_total_limit 2 \
+    --save_steps 1000 \
+    --logging_steps 10 \
+    --validation_file ${home_dir}/data/pan_tadeusz4/dataset/pan_tadeusz.syl1.txt \
+    --do_eval \
+    --evaluation_strategy 'steps' \
+    --eval_steps 1000 \
+    --per_device_eval_batch_size 800
+
+    # --disable_tqdm False
+    # --model_name_or_path ${home_dir}/runs/pan_tadeusz4/run_1/checkpoint-6000 \
