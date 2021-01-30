d4d36609285f28250b8d3f89702290a4f1e1f195
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2021-01-30 20:32:06 +0000

    run_4

--- src/experiments/pan_tadeusz4_mlm_stem/train_lm.sh
+++ src/experiments/pan_tadeusz4_mlm_stem/train_lm.sh
@@ -1,20 +1,19 @@
 home_dir=/root/poetry2021
 # home_dir=/workspace/poetry2021.gt
-run=3
+run=4
 PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
     --output_dir ${home_dir}/runs/pan_tadeusz4/run_${run} \
     --logging_dir ${home_dir}/runs/pan_tadeusz4/run_${run}_logs \
     --overwrite_output_dir \
+    --model_name_or_path "${home_dir}/runs/pan_tadeusz4/run_3 copy/checkpoint-72000" \
     --model_type roberta \
-    --config_name ${home_dir}/data/pan_tadeusz4/model_config3 \
     --tokenizer_name ${home_dir}/data/pan_tadeusz4/tokenizer \
     --max_seq_length 128 \
-    --line_by_line \
     --train_file ${home_dir}/data/pan_tadeusz4/dataset/pan_tadeusz.syl1.x100.txt \
     --do_train \
     --fp16 \
-    --learning_rate 1e-3 \
-    --per_device_train_batch_size 1000 \
+    --learning_rate 1e-4 \
+    --per_device_train_batch_size 800 \
     --num_train_epochs 650 \
     --seed 42 \
     --save_total_limit 2 \
@@ -28,3 +27,5 @@ PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
 
     # --disable_tqdm False
     # --model_name_or_path ${home_dir}/runs/pan_tadeusz4/run_1/checkpoint-6000 \
+    # --line_by_line \
+    # --config_name ${home_dir}/data/pan_tadeusz4/model_config3 \
