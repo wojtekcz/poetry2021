00d85e27ea109b00c47eba86b7389b0296b99ac1
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-31 23:54:58 +0100

    Updated

--- src/experiments/pan_tadeusz5_clm_finetuning/run_gen.sh
+++ src/experiments/pan_tadeusz5_clm_finetuning/run_gen.sh
@@ -5,4 +5,4 @@ PYTHONIOENCODING=UTF-8 python3 run_generation.py \
     --model_type=gpt2 \
     --length=200 \
     --num_return_sequences=5 \
-    --model_name_or_path=${home_dir}/runs/pan_tadeusz5/run_3/checkpoint-4000
+    --model_name_or_path=${home_dir}/runs/pan_tadeusz5/run_4/checkpoint-4000
