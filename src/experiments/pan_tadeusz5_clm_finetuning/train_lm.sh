60335849c39cc256564519eaa6531faa0ecf4938
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-01-31 17:44:44 +0000

    Setting up run_1 with Pan Tadeusz dataset

--- src/experiments/pan_tadeusz5_clm_finetuning/train_lm.sh
+++ src/experiments/pan_tadeusz5_clm_finetuning/train_lm.sh
@@ -1,8 +1,6 @@
 # home_dir=/root/poetry2021
 home_dir=/workspace/poetry2021.gt
-# run=5
-# PYTHONIOENCODING=UTF-8 python3 run_mlm.py \
-#     --output_dir ${home_dir}/runs/pan_tadeusz4/run_${run} \
+run=1
 #     --logging_dir ${home_dir}/runs/pan_tadeusz4/run_${run}_logs \
 #     --overwrite_output_dir \
 #     --model_name_or_path "${home_dir}/runs/pan_tadeusz4/run_3/" \
@@ -10,35 +8,32 @@ home_dir=/workspace/poetry2021.gt
 #     --tokenizer_name ${home_dir}/data/pan_tadeusz4/tokenizer \
 #     --max_seq_length 128 \
 #     --train_file ${home_dir}/data/pan_tadeusz4/dataset/pan_tadeusz.syl1.x100.txt \
-#     --do_train \
-#     --fp16 \
 #     --learning_rate 5e-4 \
-#     --per_device_train_batch_size 800 \
 #     --num_train_epochs 650 \
 #     --seed 42 \
 #     --save_total_limit 2 \
 #     --save_steps 1000 \
 #     --logging_steps 10 \
 #     --validation_file ${home_dir}/data/pan_tadeusz4/dataset/pan_tadeusz.syl1.txt \
-#     --do_eval \
 #     --evaluation_strategy 'steps' \
 #     --eval_steps 1000 \
 #     --per_device_eval_batch_size 800
 
-#     # --disable_tqdm False
 #     # --model_name_or_path ${home_dir}/runs/pan_tadeusz4/run_1/checkpoint-6000 \
 #     # --line_by_line \
 #     # --config_name ${home_dir}/data/pan_tadeusz4/model_config3 \
 
-# TODO: use small GPT2 model, use pan_tadeusz dataset
-python3 run_clm.py \
-    --model_name_or_path gpt2-small \
-    --dataset_name wikitext \
-    --dataset_config_name wikitext-2-raw-v1 \
+# TODO: + use small GPT2 model, + use pan_tadeusz dataset
+PYTHONIOENCODING=UTF-8 python3 run_clm.py \
+    --model_name_or_path gpt2 \
     --do_train \
-    --do_eval \
+    --train_file ${home_dir}/data/pan_tadeusz5/dataset/pan_tadeusz.txt \
     --output_dir ${home_dir}/runs/pan_tadeusz5/run_${run} \
+    --fp16 \
     --per_device_train_batch_size 3 \
     --per_device_eval_batch_size 3
 
-#    --fp16 \
+    # --do_eval \
+    # --dataset_name wikitext \
+    # --dataset_config_name wikitext-2-raw-v1 \
+    # --disable_tqdm False
