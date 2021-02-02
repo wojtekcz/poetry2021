3243eac53fd544d316576a3a0a4308c6a1fee4d9
Wojciech Czarnowski <wcz@jatar.com.pl>
2021-02-02 10:13:30 +0000

    Allow for spaces in filenames

--- src/preprocessing/stemmer.py
+++ src/preprocessing/stemmer.py
@@ -10,4 +10,4 @@ class Stemmer:
         platform_suffixes = {'Linux': 'linux', 'Darwin': 'macos'}
         platform_suffix = platform_suffixes[platform.system()]
         stemmer_bin = f'LD_PRELOAD="" /usr/local/bin/stemmer.{platform_suffix}'
-        os.system(f'{stemmer_bin} -s {s_opts} --stem_delim="{stem_delim}" -d /usr/local/bin/stemmer2.dic -i {fn_corpus_caps} -o {fn_corpus_syl}')
+        os.system(f'{stemmer_bin} -s {s_opts} --stem_delim="{stem_delim}" -d /usr/local/bin/stemmer2.dic -i "{fn_corpus_caps}" -o "{fn_corpus_syl}"')
